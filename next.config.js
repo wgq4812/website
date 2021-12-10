9be3bccea05fec030c7d90899a3c6e1ed1949e09
Oscar Beaumont <oscar@otbeaumont.me>
2021-12-11 03:22:02 +0800

    fix: :bug: upgrade Referrer-Policy header

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
+            value: 'strict-origin-when-cross-origin'
           },
           {
             key: 'Permissions-Policy',
